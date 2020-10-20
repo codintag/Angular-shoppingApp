b26f8b48890a91a5a41a716cedf77c24bde291e7
codintag <codintag@gmail.com>
2020-10-20 23:06:49 +0100

    json-server installed and data.js created in root folder for this

---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = function() {
+    return {
+        products : [
+            {id : 1, name:"Samsung galaxy s7", price: 1000, imageUrl: "1.jpg", description: "Best phone ever made!"},
+            {id : 1, name:"Samsung galaxy s7", price: 1000, imageUrl: "1.jpg", description: "Best phone ever made!"},
+            {id : 1, name:"Samsung galaxy s7", price: 1000, imageUrl: "1.jpg", description: "Best phone ever made!"},
+            {id : 1, name:"Samsung galaxy s7", price: 1000, imageUrl: "1.jpg", description: "Best phone ever made!"},
+            {id : 1, name:"Samsung galaxy s7", price: 1000, imageUrl: "1.jpg", description: "Best phone ever made!"},
+            {id : 1, name:"Samsung galaxy s7", price: 1000, imageUrl: "1.jpg", description: "Best phone ever made!"},
+        ],
+
+        categories: [
+            {id: 1, name: "Phone"},
+            {id: 2, name: "PC"},
+            {id: 3, name: "Electronique"},
+        ],
+        orders: [],
+    }
+}
